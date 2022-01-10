c90850ff31d050cf8673e46e0599d0cca64bd68f
imre972 <55357980+imre972@users.noreply.github.com>
2022-01-10 11:51:41 +0100

    Update auth.component.spec.ts

--- src/app/auth/auth.component.spec.ts
+++ src/app/auth/auth.component.spec.ts
@@ -1,6 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { AuthComponent } from './auth.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {FormsModule} from '@angular/forms'
+import {ReactiveFormsModule} from '@angular/forms';
+import {AuthComponent} from './auth.component';
 
 describe('AuthComponent', () => {
   let component: AuthComponent;
@@ -8,7 +10,12 @@ describe('AuthComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AuthComponent ]
+      declarations: [AuthComponent],
+      imports: [
+        RouterTestingModule,
+        ReactiveFormsModule,
+        FormsModule
+      ]
     })
     .compileComponents();
   }));
