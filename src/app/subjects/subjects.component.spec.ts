664295c0bcac862a019e549d262968f834a45eb6
imre972 <55357980+imre972@users.noreply.github.com>
2022-01-10 11:53:09 +0100

    Update subjects.component.spec.ts

--- src/app/subjects/subjects.component.spec.ts
+++ src/app/subjects/subjects.component.spec.ts
@@ -1,16 +1,30 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
-import { SubjectsComponent } from './subjects.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {HttpErrorHandler} from '../http-error-handler.service';
+import {SubjectsComponent} from './subjects.component';
 
 describe('SubjectsComponent', () => {
   let component: SubjectsComponent;
   let fixture: ComponentFixture<SubjectsComponent>;
 
   beforeEach(async(() => {
+    const mockErrorHandler = jasmine.createSpyObj(['createHandleError']);
+
     TestBed.configureTestingModule({
-      declarations: [ SubjectsComponent ]
+      declarations: [SubjectsComponent],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        MatDialogModule
+      ],
+      providers: [
+        {provide: HttpErrorHandler, useValue: mockErrorHandler}
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
